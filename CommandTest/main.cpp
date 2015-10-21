0a881a146dc543d439630c8e21d5f085ba53d91f
Julian Walker <julian@fiftythree.com>
2015-10-21 19:19:11 -0400

    Temp

--- CommandTest/main.cpp
+++ CommandTest/main.cpp
@@ -11,9 +11,47 @@
 
 using namespace std;
 
+class GenericCommandReceiver
+{
+public:
+    
+    template<typename ReceiverT>
+    GenericCommandReceiver(const shared_ptr<ReceiverT> &receiver)
+    {
+        _Self = make_shared<ReceiverWrapper<ReceiverT>>(receiver);
+    }
+    
+    template<typename CommandT>
+    void Execute(const shared_ptr<CommandT> &command)
+    {
+        _Self->Execute(command);
+    }
+private:
+    struct Concept {
+        virtual ~Concept() = default;
+        
+        template<typename CommandT>
+        void Execute(const shared_ptr<CommandT> &command) const;
+    };
+    template <typename ReceiverT>
+    struct ReceiverWrapper : Concept {
+        ReceiverWrapper(const shared_ptr<ReceiverT> &receiver) : _Receiver(receiver) {}
+        
+        template<typename CommandT>
+        void Execute(const shared_ptr<CommandT> &command)
+        {
+            _Receiver->Execute(command);
+        }
+        
+        shared_ptr<ReceiverT> _Receiver;
+    };
+    shared_ptr<const Concept> _Self;
+};
+
 int main(int argc, const char * argv[])
 {
     auto user = User::New(1);
+    auto space = Space::New(3);
     
     vector<std::shared_ptr<Command>> commands = {
         make_shared<InsertSpace>(1, 2),
@@ -21,8 +59,15 @@ int main(int argc, const char * argv[])
         make_shared<InsertIdea>(3, 4)
     };
     
-    for (auto command : commands) {
-        user->Execute(command);
+    vector<GenericCommandReceiver> commandReceivers = {
+        GenericCommandReceiver(user),
+        GenericCommandReceiver(space)
+    };
+    
+    for (auto receiver : commandReceivers) {
+        for (auto command : commands) {
+            receiver.Execute(command);
+        }
     }
 
     return 0;
